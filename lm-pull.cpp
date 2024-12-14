0070dc1bacfda37028d83b21ba6caf421131224c
Eric Curtin <ecurtin@redhat.com>
2024-12-14 23:32:23 +0000

    remove unused variable
    Signed-off-by: Eric Curtin <ecurtin@redhat.com>

--- lm-pull.cpp
+++ lm-pull.cpp
@@ -28,8 +28,7 @@ static std::string fmt(const char* fmt, ...) {
   const int size = vsnprintf(NULL, 0, fmt, ap);
   std::string buf;
   buf.resize(size + 1);
-  const int size2 =
-      vsnprintf(const_cast<char*>(buf.data()), buf.size(), fmt, ap2);
+  vsnprintf(const_cast<char*>(buf.data()), buf.size(), fmt, ap2);
   va_end(ap2);
   va_end(ap);
 
